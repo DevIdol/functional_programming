7c5146027d6bd4ed0d42a902a509d1cb30a66028
DevIdol <devidol.mm@gmail.com>
2022-12-16 23:08:21 +0630

    Sort Using HOF

---
+++ chapter-03/Sort.js
@@ -0,0 +1,37 @@
+let arr = [11, 100, 12, 60, 8, 7];
+const comparator = (x, y) => x - y;
+arr.sort(comparator);
+console.log("Arr Sort:", arr);
+
+console.log("==============================");
+//comparator
+//===========
+// == 0
+// > 1
+// < -1
+
+let users = [
+    {
+        name: "Mg Mg",
+        age: 33,
+    },
+    {
+        name: "Mg Mg",
+        age: 18,
+    },
+    {
+        name: "Mg Mg",
+        age: 25,
+    },
+];
+
+const sortBy = (property) => {
+    return (a, b) => {
+        let result =
+            a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+        return result;
+    };
+};
+
+users.sort(sortBy('age'));
+console.log("Users", users)
